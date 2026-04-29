3868a4fdfa981b3f771b90c6eb5475e57f771e29
dwaays <danybotch100@gmail.com>
2026-04-29 22:30:24 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
+window.onload = function() {
+  var hamburger = document.getElementById("hamburger")
+  var navLinks = document.getElementById("navLinks")
 
+  if (hamburger && navLinks) {
+    hamburger.addEventListener("click", function() {
+      navLinks.classList.toggle("open")
+    })
+
+    // Close menu when a link is clicked
+    var links = navLinks.querySelectorAll("a")
+    links.forEach(function(link) {
+      link.addEventListener("click", function() {
+        navLinks.classList.remove("open")
+      })
+    })
+  }
+}
